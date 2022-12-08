eb291b0ec823bef7615bf568aef9743c603d332e
MTM JackSon <ryantokmanmokmtm@hotmail.com>
2022-12-08 22:24:12 +0800

    update script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # shellcheck disable=SC2164
-cd /home/ec2-user/movie-server-pipeline
-docker-compose -f docker-compose_env.yaml down
+#cd /home/ec2-user/movie-server-pipeline
+#docker-compose -f docker-compose_env.yaml down
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,5 @@
 
 # shellcheck disable=SC2164
 cd /home/ec2-user/movie-server-pipeline
+docker-compose -f docker-compose_env.yaml down
 docker-compose -f docker-compose_env.yaml up
